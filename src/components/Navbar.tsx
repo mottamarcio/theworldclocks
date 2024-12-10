7d1df9970cef247a1ef8d8a022b5fa4ad8ed94f8
Marcio Motta <marciovcmotta@hotmail.com>
2024-12-10 14:00:40 -0300

    Add meta tags to index.html for a better SEO strategy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,14 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className="container d-flex justify-content-center">
-                <a className="navbar-brand text-white" href="/">
-                    <strong>The</strong>World<strong>Clocks</strong>
-                </a>
+                <h1 className="navbar-brand text-white mb-0">
+                    <a href="/" className="text-decoration-none text-white" id="brand">
+                        <strong>The</strong>World<strong>Clocks</strong>
+                    </a>
+                </h1>
             </div>
         </nav>
+
     );
 };
 
